e9bad5b2c1b3b52c881121f8dfe177404a9432b5
dathannobrega <dathanitau@gmail.com>
2025-06-06 20:11:25 -0300

    fix: update CORS middleware to allow all origins and restrict methods

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,9 +40,9 @@ def create_app() -> FastAPI:
     # Middleware de CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://www.protexion.cloud"],
+        allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=["*"],
+        allow_methods=["GET", "POST", "PUT","DELETE"],
         allow_headers=["*"],
     )
 
