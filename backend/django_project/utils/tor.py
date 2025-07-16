fa9bebedf06eb0d9941a96171e35cd6c54d6e65c
Dathan Nobrega <30958133+dathannobrega@users.noreply.github.com>
2025-07-16 14:11:20 -0300

    Fix TOR circuit renewal with hostname

--- backend/django_project/utils/tor.py
+++ backend/django_project/utils/tor.py
@@ -1,12 +1,18 @@
 from stem import Signal
 from stem.control import Controller
 from django.conf import settings
+from ipaddress import ip_address
+import socket
 
 
 def renew_tor_circuit() -> None:
     """Request a new TOR circuit using the control port."""
-    with Controller.from_port(address=settings.TOR_CONTROL_HOST,
-                              port=settings.TOR_CONTROL_PORT) as ctrl:
+    host = settings.TOR_CONTROL_HOST
+    try:
+        ip_address(host)
+    except ValueError:
+        host = socket.gethostbyname(host)
+    with Controller.from_port(address=host, port=settings.TOR_CONTROL_PORT) as ctrl:
         if settings.TOR_CONTROL_PASSWORD:
             ctrl.authenticate(password=settings.TOR_CONTROL_PASSWORD)
         else:
