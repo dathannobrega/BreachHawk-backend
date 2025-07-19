c48019510fcf45ed5e439f32cb0593a91932cf46
Dathan Nobrega <30958133+dathannobrega@users.noreply.github.com>
2025-07-19 19:01:57 -0300

    Add CRUD functionality for monitored resources

--- backend/django_project/monitoring/urls.py
+++ backend/django_project/monitoring/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from .views import MonitoredResourceListCreateView, AlertListView
+from .views import (
+    MonitoredResourceListCreateView,
+    MonitoredResourceDetailView,
+    AlertListView,
+)
 
 urlpatterns = [
     path(
         "resources/",
         MonitoredResourceListCreateView.as_view(),
         name="monitoredresource-list",
+    ),
+    path(
+        "resources/<int:pk>/",
+        MonitoredResourceDetailView.as_view(),
+        name="monitoredresource-detail",
     ),
     path("alerts/", AlertListView.as_view(), name="alert-list"),
 ]
